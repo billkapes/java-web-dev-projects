fb2d48cbdcbb2d417eb4326edb1cb2f6a8a54d7b
billkapes <kapes15@yahoo.com>
2024-09-12 19:44:39 -0500

    completed studio

---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -0,0 +1,9 @@
+package org.launchcode;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Menu {
+    Date lastUpdated = new Date();
+
+    ArrayList<MenuItem> menuItems = new ArrayList<>();
+}
---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
@@ -0,0 +1,49 @@
+package org.launchcode;
+
+public class MenuItem {
+    private float price;
+    private String description;
+    private String category;
+    private boolean isNew;
+
+    public MenuItem(float price, String description, String category, boolean isNew) {
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.isNew = isNew;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public void setNew(boolean aNew) {
+        isNew = aNew;
+    }
+
+
+}
