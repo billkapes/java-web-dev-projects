2ba9c73323c641f07dcd828dc4e6ff48a38cacef
billkapes <kapes15@yahoo.com>
2024-10-02 02:29:53 -0500

    did filename extension

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -5,16 +5,21 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
-        System.out.println(Divide(6,0));
+        //System.out.println(Divide(6,3));
 
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
         studentFiles.put("Brad", "");
         studentFiles.put("Elizabeth", "MyCode.java");
-        studentFiles.put("Stefanie", "CoolProgram.java");
+        studentFiles.put("Stefanie", "CoolProgram.js");
 
         // Test out your CheckFileExtension() function!
+        System.out.println(CheckFileExtension(studentFiles.get("Carl")));
+        System.out.println(CheckFileExtension(studentFiles.get("Brad")));
+        System.out.println(CheckFileExtension(studentFiles.get("Elizabeth")));
+        System.out.println(CheckFileExtension(studentFiles.get("Stefanie")));
+
     }
 
     public static int Divide(int x, int y)
@@ -32,6 +37,20 @@ public class Main {
 
     public static int CheckFileExtension(String fileName)
     {
-        return 0;
+        if (fileName == null || fileName.isEmpty() || fileName.length() < 5) {
+            try {
+                throw new Exception("Filename is null or empty or too short.");
+            } catch (Exception e) {
+                e.printStackTrace();
+                return -1;
+            }
+        }
+        String extension = fileName.substring(fileName.length() - 5);
+        if (extension.equals(".java")) {
+            return  1;
+        } else {
+            return  0;
+        }
+
     }
 }
